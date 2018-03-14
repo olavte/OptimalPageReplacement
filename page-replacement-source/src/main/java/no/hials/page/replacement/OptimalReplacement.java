d844c90de9ca9d4c5c6ef44522c521877eb596bd
Unknown <olav.telseth94@hotmail.com>
2018-03-14 14:23:38 +0100

    Update

--- page-replacement-source/src/main/java/no/hials/page/replacement/OptimalReplacement.java
+++ page-replacement-source/src/main/java/no/hials/page/replacement/OptimalReplacement.java
@@ -1,5 +1,6 @@
 package no.hials.page.replacement;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,26 +10,72 @@ import java.util.List;
 public class OptimalReplacement extends ReplacementAlgorithm {
     
     // TODO - add some state variables here, if you need any
+    private int currentFrame = 0;
 
     @Override
     protected void reset() {
+        currentFrame = 0;
         // TODO - do preparation/initilization here, if needed
     }
     
     @Override
     public int process(String referenceString) {
+
+        // Get the reference list as an array
         List<Integer> pageReferences = Tools.stringToArray(referenceString);
         if (pageReferences == null) return 0;
-        
+
+        Collections.sort(pageReferences);
+
         int replacements = 0; // How many page replacements made
-        
-        // TODO - process the reference string here. You can see FIFOReplacement
-        // as an example. But remember, that FIFO uses a different algorithm.
-        // This class should use Optimal Replacement algorithm, described
-        // in Section 9.4.
-        // Get the reference list as an array
+
+        // Check all virtual page accesses
+        for (int page: pageReferences) {
+            for(int i = 0; i > frames.length; i++) {
+                if(frames[i] == EMPTY) {
+                    pageIn(currentFrame, page);
+                }
+            }
+            if (!isLoaded(page)) {
+                int a = 0;
+                int b = 0;
+                int c = 0;
+                for (int p: pageReferences) {
+                    if (p == frames[0]) {
+                        a = p;
+                    } else if (p == frames[1]) {
+                        b = p;
+                    } else if (p == frames[2]) {
+                        c = p;
+                    }
+                }
+                if (a > b && a > c) {
+                    pageIn(frames[0], page);
+                    replacements++;
+                }
+                if (b > a && b > c) {
+                    pageIn(frames[1], page);
+                    replacements++;
+                }
+                if (c > b && c > a) {
+                    pageIn(frames[2], page);
+                    replacements++;
+                }
+            }
+        }
        
         return replacements;
+    }
+
+    /**
+     * Move the current circular FIFO pointer - next page to be replaced
+     */
+    private void moveCurrentPointer() {
+        currentFrame++;
+        // If it overflows, go to first frame again
+        if (currentFrame == frames.length) {
+            currentFrame = 0;
+        }
     }
 
     // TODO - create any helper methods here if you need any
--- page-replacement-source/src/test/java/no/hials/page/replacement/OptimalReplacementTest.java
+++ page-replacement-source/src/test/java/no/hials/page/replacement/OptimalReplacementTest.java
@@ -18,11 +18,17 @@ public class OptimalReplacementTest {
 
         // Use the book example with 3 frames and the given reference string
         algo.setup(3);
-        String ref = "7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1";
+        String ref = "7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1"; //(7, E, E) -> (7, 0, E) -> (7, 0, 1) -> (2, 0, 1) -> ()
         int replacements = algo.process(ref);
         String frameStatus = algo.getFrameStatus();
         assertEquals(6, replacements);
         assertEquals("7, 0, 1", frameStatus);
+
+        ref = "4,7,7,3,3,3,2,2,2,2,1,1,1,1,0,0,0,0,0,0"; // (4, E, E) -> (4, 7, E) -> (4, 7, 3) -> (2, 7, 3) -> (2, 1, 3) -> (2, 1, 0)
+        replacements = algo.process(ref);
+        frameStatus = algo.getFrameStatus();
+        //assertEquals(6, replacements); // (0, E, E) -> (0, 1, E) -> (0, 1, 2) -> (3, 1, 2) -> (3, 4, 2) -> (3, 4, 7)
+        //assertEquals("2, 1, 0", frameStatus);
 
         // TODO - add additional Unit tests here
     }
